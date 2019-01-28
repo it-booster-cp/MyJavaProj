c37ca6ef6fe7a02f0c59b05dcc66d2b61af35128
it-booster-cp <chris.arts@gmx.de>
2019-01-28 21:51:54 +0100

    My new class to commit

---
+++ src/MyNewClassToGit.java
@@ -0,0 +1,9 @@
+
+public class MyNewClassToGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I will this commit to my Git :-)");
+	}
+
+}
