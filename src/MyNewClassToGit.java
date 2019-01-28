f6ca0bbe75ee8abd38f9bba14a308a4a04964095
it-booster-cp <chris.arts@gmx.de>
2019-01-28 21:59:26 +0100

    Änderung

--- src/MyNewClassToGit.java
+++ src/MyNewClassToGit.java
@@ -3,7 +3,7 @@ public class MyNewClassToGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I will this commit to my Git :-)");
+		System.out.println("I will this commit to my Git2 :-)");
 	}
 
 }
