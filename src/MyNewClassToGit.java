8a9093f6e436ba88e85facd12501dab87cee2812
it-booster-cp <chris.arts@gmx.de>
2019-01-28 22:01:22 +0100

    Jetzt aber der Meister höchstpersönlich :-)

--- src/MyNewClassToGit.java
+++ src/MyNewClassToGit.java
@@ -3,7 +3,7 @@ public class MyNewClassToGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I will this commit to my Git2 :-)");
+		System.out.println("I will this commit to my Git2 Das hab ich geändert. Der Meister :-)");
 	}
 
 }
